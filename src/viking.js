4ff82be68f59a9f82208983fcdd1db8413e1b4d7
Francisco Romano <franromano130@gmail.com>
2023-04-16 23:36:17 +0200

    Solved lab

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,35 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor (health, strength){
+       this.health = health;
+       this.strength = strength
+
+    }
+
+ attack()  {
+    return ` el soldado ha golpeado ${this.strength}`
+ }
+ receiveDamage( ) {
+thedamage = this.receiveDamage (-this.health)
+
+ }
+ }
+
+
 
 // Viking
-class Viking {}
+class Viking extends Soldier{
+   constructor (name, health, strength){
+      super(health, strength,)
+      this.name = name
+   }
+}
 
 // Saxon
 class Saxon {}
 
 // War
 class War {}
+
+let soldier1 = new Soldier ("100", 25);
+let viking1 = new Viking ( "Ragnar", "200", 60);
